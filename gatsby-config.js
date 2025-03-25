49725f0b38a8e56e522742b79c3dc17585e987e2
geekwangc <geekwangc@163.com>
2025-03-25 23:29:58 +0800

    'init'

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
+}
